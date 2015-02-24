a60fe7bc0b753feb4297f07f2dbb9fdcd5dd7c2d
Paul Blake <paul.blake@bbc.co.uk>
2015-02-24 16:13:44 +0000

    json

--- data.js
+++ data.js
@@ -5,186 +5,186 @@ var data = [
 		"end"			: 41,
 		"image"			: "havanna-vieja2.jpg",
 		"tag"			: "Old Havana",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 
 	},
 	{
 		"start" : 46,
 		"end"	: 49,
-		"image"	: "havanCentro.jpg",
+		"image"	: "plazaVieja.jpg",
 		"tag"	: "Ornate restorations",
 		"link"  : "http://www.bbc.co.uk"
 	},
 	{
 		"start" : 54,
 		"end"	: 57,
-		"image"	: null,
+		"image"	: "havana-night2.jpg",
 		"tag"	: "Friday Night",
-		"description"	: "Lorem ipsum dolar",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	},
 	{
 		"start" : 62,
 		"end"	: 65,
-		"image"	: "null",
+		"image"	: "cafeMusicians.jpg",
 		"tag"	: "Live Cuban Music",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	},
 	{
 		"start" : 69,
 		"end"	: 72,
-		"image"	: "null",
+		"image"	: "Buenavista.jpg",
 		"tag"	: "Buena Vista Social Club",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	},
 	{
 		"start" : 73,
 		"end"	: 76,
-		"image"	: "null",
+		"image"	: "cuban-violins.jpg",
 		"tag"	: "Another side of Cuban music",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	}, 
 	{
 		"start" : 77,
 		"end"	: 80,
-		"image"	: "null",
+		"image"	: "IMG_7933.jpg",
 		"tag"	: "Come with me to a quieter part",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	},
 //SETTING
 	{
 		"start" : 83,
 		"end"	: 89,
-		"image"	: "null",
+		"image"	: "plaza_de_san_francisco.jpg",
 		"tag"	: "Plaza de San Franciso",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	},
 	{
 		"start" : 93,
 		"end"	: 96,
-		"image"	: "null",
+		"image"	: null,
 		"tag"	: "Ferry Terminal",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	}, 
 	{
 		"start" : 104,
 		"end"	: 107,
-		"image"	: "null",
+		"image"	: null,
 		"tag"	: "16th Century Mission",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	}, 
 	{
 		"start" : 114,
 		"end"	: 117,
-		"image"	: "null",
+		"image"	: null,
 		"tag"	: "Restored as a concert venue",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	},
 	{
 		"start" : 120,
 		"end"	: 123,
 		"image"	: "cameratuRomeu.jpg",
 		"tag"	: "Cameratu Romeu",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	}, 
 //INTERVIEW
 	{
 		"start" : 150,
 		"end"	: 153,
-		"image"	: "null",
+		"image"	: null,
 		"tag"	: "Zanidu Romeu",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	},  
 	{
 		"start" : 155,
 		"end"	: 158,
-		"image"	: "null",
+		"image"	: null,
 		"tag"	: "Piazolla",
-		"description"	: "null",
+		"description"	: null,
 		"link"			: "http://en.wikipedia.org/wiki/Astor_Piazzolla"
 	},
 	{
 		"start" : 159,
 		"end"	: 160,
-		"image"	: "null",
+		"image"	: null,
 		"tag"	: "1993",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	}, 
 	{
 		"start" : 161,
 		"end"	: 162,
-		"image"	: "null",
+		"image"	: null,
 		"tag"	: "Founded the orchestra",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	},
 	{
 		"start" : 165,
 		"end"	: 168,
-		"image"	: "null",
+		"image"	: null,
 		"tag"	: "female young musicians in Cuba",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	},
 	{
 		"start" : 182,
 		"end"	: 185,
-		"image"	: "null",
+		"image"	: null,
 		"tag"	: "Quite rare in other countries",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	},
 	{
 		"start" : 187,
 		"end"	: 188,
-		"image"	: "null",
+		"image"	: null,
 		"tag"	: "Our own cuban music",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	},
 	{
 		"start" : 189,
 		"end"	: 190,
-		"image"	: "null",
+		"image"	: null,
 		"tag"	: "Latin America",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	},
 	{
 		"start" : 191,
 		"end"	: 192,
-		"image"	: "null",
+		"image"	: null,
 		"tag"	: "Universal",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	},
 	{
 		"start" : 197,
 		"end"	: 200,
-		"image"	: "null",
+		"image"	: null,
 		"tag"	: "Education",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	},
 	{
 		"start" : 205,
 		"end"	: 208,
-		"image"	: "null",
+		"image"	: null,
 		"tag"	: "Surprise",
-		"description"	: "null",
-		"link"			: "null"
+		"description"	: null,
+		"link"			: null
 	}
 
 ]
