4f82071ad0a4138a956416723e7eb838a7d8f08e
Qambar Raza <razaq01@mc-1001066j24.national.core.bbc.co.uk>
2015-02-24 14:47:07 +0000

    nice fading effect

--- js/storyteller-engine.js
+++ js/storyteller-engine.js
@@ -8,25 +8,41 @@ document.addEventListener( "DOMContentLoaded", function() {
 				start   : currentFrame.start,
 				end     : currentFrame.end,
 				onStart: function( options ) {
+                    
+                    
+                    
+
 					//Change background image
-					if (currentFrame.image)
+					if (currentFrame.image) 
                         $('body').css('background-image', 'url(img/' + currentFrame.image + ')');
 					//Change the Tag
-					if (currentFrame.tag)
+					if (currentFrame.tag) {
                         $( ".tag" ).html(currentFrame.tag);
+                        $(".tag").fadeIn();
+                    }
                     //Change the description
-                    if (currentFrame.tag)
+                    if (currentFrame.tag) {
                         $( ".description" ).html(currentFrame.description);
+                        $(".description").fadeIn();
+                    }
                     //Change the link
                     if (currentFrame.link) {
-                        $( ".button" ).attr('href', currentFrame.link);
+                        var linkElements = (currentFrame.link).split("|");
+                        if (linkElements.length > 1) {
+                            $( ".button" ).attr('href', linkElements[1]);
+                            $( ".button" ).html(linkElements[0]);
+                        } else {
+                            $( ".button" ).attr('href', linkElements[0]);
+                            $( ".button" ).html(linkElements[0]);
+                        }
+                        $(".button").fadeIn();
                     }
 				},
 				onEnd: function( options ) {
                     $('body').css('background-image', 'url("")');
-                    $(".tag").html('');
-                    $(".description").html('');
-                    $(".button").attr('href', '#');
+                    $(".tag").fadeOut();
+                    $(".description").fadeOut();
+                    $(".button").fadeOut();
 				}
 			}); //end of popcorn.code
         }); //end of $.each
