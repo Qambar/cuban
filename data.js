ad5daf8a92d25b7c901faa13a44d1d1c5197bd8e
Qambar Raza <razaq01@mc-1001066j24.national.core.bbc.co.uk>
2015-02-24 15:20:48 +0000

    removed data.json

--- data.js
+++ data.js
@@ -2,8 +2,6 @@ var data = [
 	{
 		"start" 		: 1,
 		"end"			: 3,
-		"image"			: "angelVasquesMillares.jpg",
-		"tag"			: "Amazing",
 		"description"	: "Lorem ipsum dolar",
 		"link"			: "BBC|http://www.bbc.co.uk"
 
@@ -18,7 +16,6 @@ var data = [
 	{
 		"start" : 10,
 		"end"	: 15,
-		"image"	: null,
 		"tag"	: "Paul Blake's request",
 		"description"	: "Lorem ipsum dolar",
 		"link"			: "http://www.bbc.co.uk"
