2bcb6c5ad95b24e80afe9f99502f6e121791167e
Qambar Raza <razaq01@mc-1001066j24.national.core.bbc.co.uk>
2015-02-24 14:39:24 +0000

    added more features

--- js/storyteller-engine.js
+++ js/storyteller-engine.js
@@ -14,9 +14,19 @@ document.addEventListener( "DOMContentLoaded", function() {
 					//Change the Tag
 					if (currentFrame.tag)
                         $( ".tag" ).html(currentFrame.tag);
+                    //Change the description
+                    if (currentFrame.tag)
+                        $( ".description" ).html(currentFrame.description);
+                    //Change the link
+                    if (currentFrame.link) {
+                        $( ".button" ).attr('href', currentFrame.link);
+                    }
 				},
 				onEnd: function( options ) {
-                    
+                    $('body').css('background-image', 'url("")');
+                    $(".tag").html('');
+                    $(".description").html('');
+                    $(".button").attr('href', '#');
 				}
 			}); //end of popcorn.code
         }); //end of $.each
