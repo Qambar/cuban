a03f3eeea4d6b7604262b36669667f158b690fdf
Paul Blake <paul.blake@bbc.co.uk>
2015-02-24 15:31:08 +0000

    updated json, new image

--- data.js
+++ data.js
@@ -1,26 +1,117 @@
 var data = [
+//INTRO
 	{
-		"start" 		: 1,
-		"end"			: 3,
+		"start" 		: 39,
+		"end"			: 41,
 		"image"			: "angelVasquesMillares.jpg",
-		"tag"			: "Amazing",
+		"tag"			: "Old Havana",
 		"description"	: "Lorem ipsum dolar",
-		"link"			: "BBC|http://www.bbc.co.uk"
+		"link"			: "BBC|http://www.bbc.co.uk*"
 
 	},
 	{
-		"start" : 4,
-		"end"	: 9,
-		"image"	: "balletSchool.jpg",
-		"tag"	: "Wohoo!",
+		"start" : 46,
+		"end"	: 49,
+		"image"	: "havanCentro.jpg",
+		"tag"	: "Ornate restorations",
 		"link"  : "http://www.bbc.co.uk"
 	},
 	{
-		"start" : 10,
-		"end"	: 15,
+		"start" : 54,
+		"end"	: 57,
 		"image"	: null,
-		"tag"	: "Paul Blake's request",
+		"tag"	: "Friday Night",
 		"description"	: "Lorem ipsum dolar",
-		"link"			: "http://www.bbc.co.uk"
-	}        		
+		"link"			: "null"
+	} 
+	{
+		"start" : 62,
+		"end"	: 65,
+		"image"	: "null",
+		"tag"	: "Live Cuban Music",
+		"description"	: "null",
+		"link"			: "null"
+	}
+	{
+		"start" : 69,
+		"end"	: 72,
+		"image"	: "null",
+		"tag"	: "Buena Vista Social Club",
+		"description"	: "null",
+		"link"			: "null"
+	}
+	{
+		"start" : 73,
+		"end"	: 76,
+		"image"	: "null",
+		"tag"	: "Another side of Cuban music",
+		"description"	: "null",
+		"link"			: "null"
+	} 
+	{
+		"start" : 77,
+		"end"	: 80,
+		"image"	: "null",
+		"tag"	: "Come with me to a quieter part",
+		"description"	: "null",
+		"link"			: "null"
+	}
+//SETTING
+	{
+		"start" : 83,
+		"end"	: 89,
+		"image"	: "null",
+		"tag"	: "Plaza de San Franciso",
+		"description"	: "null",
+		"link"			: "null"
+	} 
+	{
+		"start" : 93,
+		"end"	: 96,
+		"image"	: "null",
+		"tag"	: "Ferry Terminal",
+		"description"	: "null",
+		"link"			: "null"
+	}  
+	{
+		"start" : 104,
+		"end"	: 107,
+		"image"	: "null",
+		"tag"	: "16th Century Mission",
+		"description"	: "null",
+		"link"			: "null"
+	} 
+	{
+		"start" : 114,
+		"end"	: 117,
+		"image"	: "null",
+		"tag"	: "Restored as a concert venue",
+		"description"	: "null",
+		"link"			: "null"
+	}
+	{
+		"start" : 120,
+		"end"	: 123,
+		"image"	: "cameratuRomeu.jpg",
+		"tag"	: "Cameratu Romeu",
+		"description"	: "null",
+		"link"			: "null"
+	} 
+//INTERVIEW
+	{
+		"start" : 150,
+		"end"	: 153,
+		"image"	: "null",
+		"tag"	: "Zanidu Romeu",
+		"description"	: "null",
+		"link"			: "null"
+	}  
+	{
+		"start" : 155,
+		"end"	: 65,
+		"image"	: "null",
+		"tag"	: "Piazolla",
+		"description"	: "null",
+		"link"			: "null"
+	} 		
 ]
